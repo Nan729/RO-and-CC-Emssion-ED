dba688c6190bcacf7f4195390e1a0aeadb457ab9
Nan729 <81351851+Nan729@users.noreply.github.com>
2021-05-20 21:36:30 +0800

    Delete code directory

--- code/gen_samples.m
+++
@@ -1,6 +0,0 @@
-function [gm] = gen_samples(error_data)
-%UNTITLED3 此处显示有关此函数的摘要
-%   此处显示详细说明
-    gm = fitgmdist(error_data,2);
-end
-
--- code/gen_wind_data.m
+++
@@ -1,9 +0,0 @@
-function[wind_error]= gen_wind_data(real_capacity_scale,filename)
-    %filename='.\data\WindGenTotalLoadYTD_2020.xls';
-    input_data= xlsread(filename, 1, 'B25:C52428');
-    input_scale=mean(input_data(:,1));
-    wind_data=input_data./input_scale*real_capacity_scale;
-    wind_error=wind_data(:,2)-wind_data(:,1);
-%     error_norm=normalize(error_data,'scale');
-%     wind_error=error_norm*ratio;
-end
